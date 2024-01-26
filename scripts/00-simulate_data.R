b87c23db20e27b1149f95edc3036dcde4bc7d9bb
CatherinePunnoose <152019416+catherinee1216@users.noreply.github.com>
2024-01-25 23:14:06 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,30 @@
 #### Preamble ####
 # Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Catherine Punnoose
+# Date: 24 January 2024 
+# Contact: catherine.punnoose@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: opendatatoronto
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+simulated_data <-
+  tibble(
+    # Use 1 through to 151 to represent each division
+    "id" = 1:151,
+    # Randomly pick an option, with replacement, 151 times
+    "Population Group" = sample(
+      x = c("All Populations", "Chronic", "Families", "Refugees", "Non-Refugees", "Single Adult", "Youth"),
+      size = 151,
+      replace = TRUE
+    )
+  )
+
+simulated_data
 
 
 
